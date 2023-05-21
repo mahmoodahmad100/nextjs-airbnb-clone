964277112eed698b2c1f772aebe88683f71d6abb
Mahmood Ahmad <mahmood073@gmail.com>
2023-05-21 18:08:50 +0300

    init listing list + favorit functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,51 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
-const inter = Inter({ subsets: ['latin'] })
+import getListings, { 
+  IListingsParams
+} from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export default function Home() {
+interface HomeProps {
+  searchParams: IListingsParams
+};
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <EmptyState showReset />
+    );
+  }
+
   return (
-    <div>
-      <h2>Hello World!</h2>
-      <p className="text-sky-400/100">Testing tailwindcss!</p>
-    </div>
+    <Container>
+      <div 
+        className="
+          pt-24
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {listings.map((listing: any) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
   )
 }
+
+export default Home;
